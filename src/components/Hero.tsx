f2927b0a7624f156e5b2780c7e1900b8ee7056d8
Yujen Su <105326785+yujensy@users.noreply.github.com>
2025-08-02 06:11:22 +0000

    feat: setup prettier and eslint and adjust content and icon

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { Container } from "@/components/Container";
-import heroImg from "../../public/img/hero.png";
+import goutMan from "../../public/img/gout-man.png"
 
 export const Hero = () => {
   return (
@@ -9,15 +9,13 @@ export const Hero = () => {
         <div className="flex items-center w-full lg:w-1/2">
           <div className="max-w-2xl mb-8">
             <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
-              Free Landing Page Template for startups
+              What most doctors never tell you about managing gout…
             </h1>
             <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
-              Nextly is a free landing page & marketing website
-              template for startups and indie projects. Its built with
-              Next.js & TailwindCSS. And its completely open-source.
+              Discover a simple food-based strategy that’s helped thousands manage gout naturally — without giving up your life.
             </p>
 
-            <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
+            {/* <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
               <a
                 href="https://web3templates.com/templates/nextly-landing-page-template-for-startups"
                 target="_blank"
@@ -43,21 +41,18 @@ export const Hero = () => {
                 </svg>
                 <span> View on Github</span>
               </a>
-            </div>
+            </div> */}
           </div>
         </div>
         <div className="flex items-center justify-center w-full lg:w-1/2">
-          <div className="">
             <Image
-              src={heroImg}
-              width="616"
-              height="617"
-              className={"object-cover"}
+              src={goutMan}
+              width={616}
+              height={617}
               alt="Hero Illustration"
               loading="eager"
               placeholder="blur"
             />
-          </div>
         </div>
       </Container>
       <Container>
--- src/components/data.js
+++ src/components/data.js
@@ -1,3 +1,4 @@
+// @ts-check
 import {
   FaceSmileIcon,
   ChartBarSquareIcon,
@@ -6,27 +7,49 @@ import {
   AdjustmentsHorizontalIcon,
   SunIcon,
 } from "@heroicons/react/24/solid";
+import feetHurtIcon from "../../public/icon/gout.png";
 
 import benefitOneImg from "../../public/img/benefit-one.png";
 import benefitTwoImg from "../../public/img/benefit-two.png";
+import Image from "next/image";
+import React from "react";
 
 const benefitOne = {
-  title: "Highlight your benefits",
+  title: "Do any of these sound familiar?",
   desc: "You can use this space to highlight your first benefit or a feature of your product. It can also contain an image or Illustration like in the example along with some bullet points.",
   image: benefitOneImg,
   bullets: [
     {
-      title: "Understand your customers",
+      title: "Sudden, stabbing pain in your feet or big toe — out of nowhere",
       desc: "Then explain the first point breifly in one or two lines.",
-      icon: <FaceSmileIcon />,
+      icon: (
+        <Image
+          alt="gout-feet-icon"
+          src={feetHurtIcon}
+          style={{ filter: "invert(1)" }}
+        />
+      ),
     },
     {
-      title: "Improve acquisition",
+      title:
+        "You’ve tried cutting out meat, beer, and even fun — but still no relief",
       desc: "Here you can add the next benefit point.",
       icon: <ChartBarSquareIcon />,
     },
     {
-      title: "Drive customer retention",
+      title:
+        "Your meds help a little, but the side effects are worse than the gout",
+      desc: "This will be your last bullet point in this section.",
+      icon: <CursorArrowRaysIcon />,
+    },
+    {
+      title: "You’re tired of living in fear of the next painful flare-up",
+      desc: "This will be your last bullet point in this section.",
+      icon: <CursorArrowRaysIcon />,
+    },
+    {
+      title:
+        "You feel alone — like no one really gets what you’re going through",
       desc: "This will be your last bullet point in this section.",
       icon: <CursorArrowRaysIcon />,
     },
@@ -56,5 +79,4 @@ const benefitTwo = {
   ],
 };
 
-
-export {benefitOne, benefitTwo};
+export { benefitOne, benefitTwo };
